7169214ddd53bfc79e681fb5f8d1180cebbe05a4
Александър Куртаков <akurtakov@gmail.com>
2025-10-31 10:29:36 +0200

    Update Tree.java to fix double-click focus issue

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -1,7 +1,27 @@
-// New double-click selection handler code for gtk_gesture_press_event
+@Override
+int gtk_gesture_press_event(long gesture, int n_press, double x, double y, long event) {
+    int result = super.gtk_gesture_press_event(gesture, n_press, x, y, event);
 
-// Handler for the GTK gesture press event
-void on_gesture_press_event(GtkEvent event) {
-    // Logic to ensure correct focus/select item under mouse
-    // ... (implementation details)
+    // Fix: Ensure double-click always focuses and selects the item under the mouse
+    if (n_press == 2) {
+        if (!isFocusControl()) setFocus();
+
+        long[] path = new long[1];
+        if (GTK.gtk_tree_view_get_path_at_pos(handle, (int)x, (int)y, path, null, null, null)) {
+            if (path[0] != 0) {
+                long selection = GTK.gtk_tree_view_get_selection(handle);
+                OS.g_signal_handlers_block_matched(selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
+                GTK.gtk_tree_view_set_cursor(handle, path[0], 0, false);
+                OS.g_signal_handlers_unblock_matched(selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
+                GTK.gtk_tree_path_free(path[0]);
+            }
+        }
+    }
+
+    if (n_press == 2 && rowActivated) {
+        sendTreeDefaultSelection();
+        rowActivated = false;
+    }
+
+    return result;
 }
